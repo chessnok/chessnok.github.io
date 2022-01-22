9a959f641d2788a209361245fd7b1f1f42c4d563
mikha <mfadin@sch1541.moscow>
2022-01-22 13:42:52 +0300

    test

--- gen.js
+++ gen.js
@@ -3,8 +3,8 @@ let start = new Date()
 let data = JSON.parse(fs.readFileSync('./data.json'))
 function gen() {
     let res = {};
-    let i=3000000;
-    while ( i<4000000) {
+    let i=3100000;
+    while ( i<3200000) {
         res[i] = 0;
         let g=0;
         while (g<=i/2) {
@@ -13,10 +13,10 @@ function gen() {
             }
             g++;
         }
+        console.log(i)
         i++;
     }
     return res;
 }
-gen()
 fs.writeFileSync("./data.json", JSON.stringify(Object.assign(data, gen())))
 console.log((new Date() -start)/1000)
